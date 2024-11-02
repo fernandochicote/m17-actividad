7633e5265fa15ea6902a241446f617db9a05c59e
fernandochicote <fechicot@gmail.com>
2024-11-02 08:06:37 +0100

    Revert "Tercer commit"
    This reverts commit 880f080fae4fc2c4fc071b0a18d9a344646fd9aa.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+
+
 import dice
 from time import sleep
+
 
 def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5,6)):
-    print(f'\u2022 Lanzamiento número {idx+1}: \nEl número obtenido es {result}')
+    print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
