880f080fae4fc2c4fc071b0a18d9a344646fd9aa
fernandochicote <fechicot@gmail.com>
2024-11-02 08:05:58 +0100

    Tercer commit

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-
-
 import dice
 from time import sleep
-
 
 def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5,6)):
-    print(f'Lanzamiento {idx+1} número obtenido {result}')
+    print(f'\u2022 Lanzamiento número {idx+1}: \nEl número obtenido es {result}')
     sleep(5)
